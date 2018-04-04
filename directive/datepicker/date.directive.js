faa464656a2c4e34a60d2b0a349b168ba806746b
taxilng <769967440@qq.com>
2018-04-04 16:09:49 +0800

    日期选择器完成

--- directive/datepicker/date.directive.js
+++ directive/datepicker/date.directive.js
@@ -6,49 +6,35 @@
 
     function timepicker($timeout) {
         var directive = {
-            restrict: 'AE',
+            require: '?ngModel',
+            restrict: 'E',
             templateUrl: '../directive/datepicker/date.html',
             scope: {
-                time: '=',
-                readonly: '=',
-                maxTime: '=',
-                minTime: '=',
+                ngModel: '='
+            },
+            controller: function ($scope) {
+
             },
             link: linkfun
         };
 
-        function linkfun(scope, elem, attrs) {
-            // scope.$watch('time',function(time){
-            //     console.log('变化');
-                
-            // })
-            console.log(scope);
-            scope.$watch('elem',function(time){
-                console.log('selTime变化');
-            },true)
-            // timeout internals are called once directive rendering is complete
-            $timeout(function () {
-
+        function linkfun(scope, elem, attrs, ngModel) {
+            $timeout(function () {   
                 $(elem).children().datetimepicker({
-                    format: attrs.format,
-                    autoclose: true,
-                    todayBtn: true,
-                    minuteStep: 1,
-                    language: 'fr', //中文
-                    forceParse: false, //选择框取消后不允许修改
-                    todayHighlight: true, //高亮显示今天日期
-                    viewSelect: 'year',
-                    format: 'yyyy-mm-dd hh:ii'
-                    // format:'yyyy-mm-dd hh:ii:ss'
-                })
-                .on('hide', function () {
-                    // console.log(scope);
-                    console.log(scope);
-                    var _this = $(this.children[0]);
-                    // console.log(_this.val());
-                    var val = new Date(_this.val()).getTime();
-                    scope.time = val
-                });
+                        format: attrs.format,
+                        autoclose: true,
+                        todayBtn: true,
+                        minuteStep: 1,
+                        language: 'zh-CN', //中文
+                        forceParse: false, //选择框取消后不允许修改
+                        todayHighlight: true, //高亮显示今天日期
+                        viewSelect: 'year',
+                        format: 'yyyy-mm-dd hh:ii'
+                    })
+                    .on('change', function () {
+                        var _this = $(this.children[0]);
+                        ngModel.$setViewValue(_this.val());
+                    });
             });
         }
         return directive
